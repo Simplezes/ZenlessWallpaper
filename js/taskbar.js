8a61b1b4be911847b622a0633ab3c3c820362d86
Simplezes <thehunterloki@gmail.com>
2026-03-31 13:10:39 -0500

    feat: added rotating and flip to character and widescreen support

--- js/taskbar.js
+++ js/taskbar.js
@@ -1,8 +1,12 @@
 function detectTaskbar() {
     const screenHeight = window.screen.height;
     const availHeight = window.screen.availHeight;
-    const taskbarHeight = screenHeight - availHeight;
-    if (window.screen.availTop === 0) {
+    let taskbarHeight = screenHeight - availHeight;
+
+    if (taskbarHeight < 0) taskbarHeight = 0;
+    if (taskbarHeight > 200) taskbarHeight = 0;
+
+    if (window.screen.availTop === 0 && taskbarHeight > 0) {
         document.documentElement.style.setProperty('--taskbar-offset', window.pxToCurrentRem(taskbarHeight));
     } else {
         document.documentElement.style.setProperty('--taskbar-offset', '0rem');
