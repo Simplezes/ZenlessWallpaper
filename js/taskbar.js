ee1d0ba8048aa6cc2172d1b4690a469111ca7357
Simplezes <thehunterloki@gmail.com>
2026-04-01 16:01:07 -0500

    feat: implement taskbar-aware layout components and dynamic screen detection for wallpaper styling

--- js/taskbar.js
+++ js/taskbar.js
@@ -1,27 +1,30 @@
 function detectTaskbar() {
-    const screenWidth = window.screen.width;
-    const screenHeight = window.screen.height;
-    const availWidth = window.screen.availWidth;
-    const availHeight = window.screen.availHeight;
-    const availLeft = window.screen.availLeft || 0;
-    const availTop = window.screen.availTop || 0;
+    const sw = window.screen.width;
+    const sh = window.screen.height;
+    const aw = window.screen.availWidth;
+    const ah = window.screen.availHeight;
+    const al = window.screen.availLeft || 0;
+    const at = window.screen.availTop || 0;
+
+    const sl = window.screenLeft !== undefined ? window.screenLeft : window.screenX;
+    const st = window.screenTop !== undefined ? window.screenTop : window.screenY;
 
     let offsetBottom = 0;
     let offsetLeft = 0;
     let offsetTop = 0;
     let offsetRight = 0;
 
-    if (availTop > 0) offsetTop = availTop;
-    if (availTop + availHeight < screenHeight) offsetBottom = screenHeight - (availTop + availHeight);
+    if (at > st) offsetTop = at - st;
+    if (at + ah < st + sh) offsetBottom = (st + sh) - (at + ah);
 
-    if (availLeft > 0) offsetLeft = availLeft;
-    if (availLeft + availWidth < screenWidth) offsetRight = screenWidth - (availLeft + availWidth);
+    if (al > sl) offsetLeft = al - sl;
+    if (al + aw < sl + sw) offsetRight = (sl + sw) - (al + aw);
 
-    const limit = 200;
-    if (offsetBottom > limit) offsetBottom = 0;
-    if (offsetTop > limit) offsetTop = 0;
-    if (offsetLeft > limit) offsetLeft = 0;
-    if (offsetRight > limit) offsetRight = 0;
+    const limit = 250;
+    if (offsetBottom > limit || offsetBottom < 0) offsetBottom = 0;
+    if (offsetTop > limit || offsetTop < 0) offsetTop = 0;
+    if (offsetLeft > limit || offsetLeft < 0) offsetLeft = 0;
+    if (offsetRight > limit || offsetRight < 0) offsetRight = 0;
 
     const root = document.documentElement;
     root.style.setProperty('--taskbar-bottom', window.pxToCurrentRem(offsetBottom));
